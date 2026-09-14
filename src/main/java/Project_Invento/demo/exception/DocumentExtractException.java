0dcce6636ff67ec8266dfac9cb1e2bd63de3dd9b
Daniel <Danttaz11@gmail.com>
2026-09-13 21:05:15 -0300

    feat: cria exception custom

---
+++ src/main/java/Project_Invento/demo/exception/DocumentExtractException.java
@@ -0,0 +1,7 @@
+package Project_Invento.demo.exception;
+
+public class DocumentExtractException extends RuntimeException {
+    public DocumentExtractException(String message, Throwable cause) {
+        super(message);
+    }
+}
